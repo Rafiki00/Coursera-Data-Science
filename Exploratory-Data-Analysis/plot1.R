0e663090bb2bb86f1692e71acc30b79e06291b8b
Rafael Llopis <rafa94el@gmail.com>
2019-06-14 08:15:16 +0200

    deleted

--- Exploratory-Data-Analysis/plot1.R
+++
@@ -1,6 +0,0 @@
-power <- read.table("household_power_consumption.txt", nrows=2880, skip=66637, sep=";", na.strings = "?")
-names(power) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
-
-png("plot1.png", width=480, height=480)
-hist(power$Global_active_power, col=2, xlab="Global active power (kilowatts)", main="Global Active Power")
-dev.off()
